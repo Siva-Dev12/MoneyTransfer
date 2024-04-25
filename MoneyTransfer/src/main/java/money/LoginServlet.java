0089d263805b0f48f76105ce3c6f0dee8119348f
Siva-Dev12 <sivakumarmadhu18@gmail.com>
2024-04-25 13:00:00 +0530

    feat:Connect to backend but not full filled

--- MoneyTransfer/src/main/java/money/LoginServlet.java
+++ MoneyTransfer/src/main/java/money/LoginServlet.java
@@ -1,5 +1,8 @@
 package money;
 
-public class LoginServlet {
+import javax.servlet.http.HttpServlet;
 
+public class LoginServlet extends HttpServlet {
+	private static final String Classname="com.mysql.cj.jdbc.Driver";
+	private static final String Db_url="jdbc:mysql://localhost:3306/moneytransfer?user=root&password=root";
 }
