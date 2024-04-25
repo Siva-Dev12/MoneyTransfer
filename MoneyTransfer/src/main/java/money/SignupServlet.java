99dfeadeb99be62c90d301bcb209ba1c3164506e
Siva-Dev12 <sivakumarmadhu18@gmail.com>
2024-04-25 13:01:08 +0530

    initial commit for signup

---
+++ MoneyTransfer/src/main/java/money/SignupServlet.java
@@ -0,0 +1,21 @@
+package money;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SignupServlet extends HttpServlet{
+
+	private static final String Classname="com.mysql.cj.jdbc.Driver";
+	private static final String Db_url="jdbc:mysql://localhost:3306/moneytransfer?user=root&password=root";
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String useremail=req.getParameter("username");
+		String userpassword=req.getParameter("userpass");
+		
+	}
+}
