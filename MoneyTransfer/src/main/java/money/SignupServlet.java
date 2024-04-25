c8b3465350d88dbbd37d1406432e96f4bc9658e7
Siva-Dev12 <sivakumarmadhu18@gmail.com>
2024-04-25 23:12:21 +0530

    feat: frontend &backend connectivity done

--- MoneyTransfer/src/main/java/money/SignupServlet.java
+++ MoneyTransfer/src/main/java/money/SignupServlet.java
@@ -1,12 +1,20 @@
 package money;
 
 import java.io.IOException;
+import java.net.ResponseCache;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/signup")
 public class SignupServlet extends HttpServlet{
 
 	private static final String Classname="com.mysql.cj.jdbc.Driver";
@@ -14,8 +22,33 @@ public class SignupServlet extends HttpServlet{
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String useremail=req.getParameter("username");
+		String useremail=req.getParameter("useremail");
 		String userpassword=req.getParameter("userpass");
 		
+		try {
+			Class.forName(Classname);
+			Connection c=DriverManager.getConnection(Db_url);
+			
+			//check if the useremail already exists
+			PreparedStatement ps=c.prepareStatement("select count(*) from user_table where usermail=?");
+			ps.setString(1,useremail);
+			ResultSet rs= ps.executeQuery();
+			
+			if(rs.next() && rs.getInt(1)>0)
+			{
+				resp.getWriter().println("useremil already exists");
+				return;
+			}
+			
+			//insert the new user
+			PreparedStatement ps1=c.prepareStatement("insert into user_table (usermail,userpassword) values(?,?)");
+			ps1.setString(1, useremail);
+			ps1.setString(2, userpassword);
+			
+			ps1.executeUpdate();
+			resp.getWriter().println("Signup successfull");
+		} catch (Exception e) {
+			resp.getWriter().println("Error :"+e.getMessage());
+		}
 	}
 }
